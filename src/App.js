b4e2dfc7dff7a91aa7a72557e8c83bcfb0227eb6
Satournine <tunaorkun@outlook.com>
2023-11-10 20:51:52 +0300

    budget tracker react app

--- src/App.js
+++ src/App.js
@@ -3,8 +3,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Budget from "./components/Budget";
 import Remaining from "./components/Remaining";
 import ExpenseTotal from "./components/ExpenseTotal";
+import ExpenseList from "./components/ExpenseList";
+import AddExpenseForm from "./components/AddExpenseForm";
+import { AppProvider } from "./context/AppContext";
+
+
 const App = () => {
   return (
+    <AppProvider>
+
     <div className="container">
       <h1 className="mt-3">Budget Planner</h1>
       <div className="row mt-3">
@@ -18,7 +25,20 @@ const App = () => {
           <ExpenseTotal />
         </div>
       </div>
-    </div>
+      <h3 className="mt-3">Expenses</h3>
+      <div className="row mt-3">
+        <div className="col-sm">
+          <ExpenseList />
+        </div>
+        </div>
+        <h3 className="mt-3">Add Expense</h3>
+        <div className="mt-3">
+          <div className="col-sm">
+            <AddExpenseForm/>
+          </div>
+        </div>
+      </div>
+      </AppProvider>
   );
 };
 
